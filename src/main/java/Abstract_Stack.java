95e83882aeb8c3cbbe61b75b89a08583745c3467
Nils Fischer <Nils-Fischer7@web.de>
2022-04-22 13:17:20 +0200

    Stack Implementation mit Iterator

--- src/main/java/Abstract_Stack.java
+++ src/main/java/Abstract_Stack.java
@@ -1,10 +1,10 @@
 import java.util.Iterator;
 
 public interface Abstract_Stack extends Iterable<Integer>{
-    boolean empty();
-    int peek();
-    int pop();
-    void push(int data);
-    int search(int data);
-    Iterator<Integer> iterator();
+    boolean empty();  // überprüft ob der Stack leer ist
+    int peek();  // schaut sich das oberste Item im Stack an, ohne es zu entfernen
+    int pop();  // entfernt das oberste Item und gibt es zurück
+    void push(int data);  // fügt ein Item zum Stack hinzu
+    int search(int data);  // sucht nach einem Item im Stack und gibt seine Position, von oben, zurück
+    Iterator<Integer> iterator();  // Iterator
 }
