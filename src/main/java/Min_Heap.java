ec91db1563ec1ee5b7a026e1a9557c900ea6912b
Ian Finn Garich <s6iagari@uni-bonn.de>
2022-05-03 11:29:32 +0000

    Update Min_Heap.java

--- src/main/java/Min_Heap.java
+++ src/main/java/Min_Heap.java
@@ -1,31 +1,31 @@
 import java.util.Iterator;
 
-public class Min_Heap implements Abstract_Min_Heap {
+public class Min_Heap implements Abstract_Min_Heap {	//Implementierung der Klasse Min_Heap, implementiert alle Methoden des Interfaces
 
     private int[] Heap;
     private int size;
     private int maxsize;
     private static final int FRONT = 1;
 
-    public Min_Heap(int maxsize) {
+    public Min_Heap(int maxsize) {	//Konstruktor
         this.maxsize = maxsize;
         this.size = 0;
         Heap = new int[this.maxsize + 1];
     }
 
-    public int parent(int pos) {
+    public int parent(int pos) {	//Methode um die Position des Eltern-Knotens des aktuellen Knotens zu ermitteln
         return pos / 2;
     }
 
-    public int left(int pos) {
+    public int left(int pos) {		//Methode um die Position des linken Tochter-Knotens des aktuellen Knotens zu ermitteln
         return (2 * pos);
     }
 
-    public int right(int pos) {
+    public int right(int pos) {		//Methode um die Position des rechten Tochter-Knotens des aktuellen Knotens zu ermitteln
         return (2 * pos) + 1;
     }
 
-    public boolean isleaf(int pos) {
+    public boolean isleaf(int pos) {		//Methode zum überprüfen, ob der aktuelle Knoten ein Leaf ist
         if (pos > (size / 2) && pos <= size) {
             return true;
         }
@@ -34,7 +34,7 @@ public class Min_Heap implements Abstract_Min_Heap {
         }
     }
 
-    public void insert(int element) {
+    public void insert(int element) {		//Methode zum einfügen eines Knotens
         if (size >= maxsize) {
             return;
         }
@@ -46,7 +46,7 @@ public class Min_Heap implements Abstract_Min_Heap {
         }
     }
 
-    private void swap(int fpos, int spos)
+    private void swap(int fpos, int spos)	//Methode zum tauschen zweier Knoten
     {
 
         int tmp;
@@ -56,14 +56,14 @@ public class Min_Heap implements Abstract_Min_Heap {
         Heap[spos] = tmp;
     }
 
-    public void print() {
+    public void print() {		//Methode zum printen des Heaps
         for (int i = 1; i <= size / 2; i++) {
             System.out.print(" PARENT : " + Heap[i] + " LEFT CHILD : " + Heap[2 * i] + " RIGHT CHILD :" + Heap[2 * i + 1]);
             System.out.println();
         }
     }
 
-    public static void main(String[] arg) {
+    public static void main(String[] arg) {	//Main-Method mit Testwerten
         Min_Heap minHeap = new Min_Heap(15);
 
         minHeap.insert(5);
